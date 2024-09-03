beb7646f4e269f1e1dc9081694e4b59e00ed9e9b
Sweety <sweety.x.ext@wsa.com>
2024-09-03 18:59:50 +0530

    web app

---
+++ WebApp/WebApp/Controllers/StudentController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ReactAspCrud.Models;
+
+namespace ReactAspCrud.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        private readonly string filePath = Path.Combine(Directory.GetCurrentDirectory(), "C:\\Users\\z004v59b\\source\\repos\\NameForm\\React-WebApp\\Name File.txt"); // Path to your text file
+
+
+
+
+        [HttpPost]
+        [Route("GetName")]
+        public async Task<IActionResult> Post([FromBody] NameModels nameModel)
+        {
+            if (nameModel == null || string.IsNullOrEmpty(nameModel.FirstName) || string.IsNullOrEmpty(nameModel.LastName))
+            {
+                return BadRequest("Invalid data.");
+            }
+
+            var concatenatedName = $"{nameModel.FirstName} {nameModel.LastName}";
+            await System.IO.File.AppendAllTextAsync(filePath, concatenatedName + System.Environment.NewLine);
+
+            return Ok("Name concatenated and saved.");
+        }
+
+    }
+}
+
+
+
---
+++ WebApp/WebApp/Models/NameModels.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ReactAspCrud.Models
+{
+    public class NameModels
+    {
+        [Key]
+        public int Id { get; set; }
+
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+    }
+}
